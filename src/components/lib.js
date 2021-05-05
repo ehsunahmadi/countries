7006e4490dfb66c7cc0b16e48d06d1e0fb7cb3e9
ehsan ahmadi <ehsun.ahmadi@gmail.com>
2021-05-06 00:47:26 +0430

    used colorMode in appBar & Message

--- src/components/lib.js
+++ src/components/lib.js
@@ -7,24 +7,32 @@ import {
   Center,
   Container,
   Flex,
-  Grid,
   Heading,
   Link,
   Spacer,
   Spinner,
+  useColorMode,
+  useTheme,
 } from "@chakra-ui/react";
 import { Link as RouterLink } from "react-router-dom";
 import ColorModeToggler from "./ColorModeToggler";
 
 export const Message = ({ title, description, status }) => {
+  const { colors } = useTheme();
+  const { colorMode } = useColorMode();
   return (
-    <Box my={2}>
-      <Alert status={status} borderRadius={10}>
-        <AlertIcon />
-        <AlertTitle mr={2}>{title}</AlertTitle>
-        {!!description && <AlertDescription>{description}</AlertDescription>}
-      </Alert>
-    </Box>
+    <Alert
+      status={status}
+      borderRadius={10}
+      width="xl"
+      backgroundColor={
+        colorMode === "dark" ? colors.darkBlue : colors.lightGray
+      }
+    >
+      <AlertIcon />
+      <AlertTitle mr={2}>{title}</AlertTitle>
+      {!!description && <AlertDescription>{description}</AlertDescription>}
+    </Alert>
   );
 };
 
@@ -42,18 +50,43 @@ export const LoadingSpinner = () => {
   );
 };
 
-export const AppBar = () => (
-  <Flex alignItems="center" height={78}>
-    <Box>
-      <Link as={RouterLink} to="/">
-        <Heading as="h1" size="md">
-          Where in the world?
-        </Heading>
-      </Link>
-    </Box>
-    <Spacer />
-    <Box>
-      <ColorModeToggler />
-    </Box>
-  </Flex>
-);
+export const AppBar = () => {
+  const {
+    sizes: { appBarHeight, maxWidth },
+    colors,
+  } = useTheme();
+  const { colorMode } = useColorMode();
+
+  return (
+    <>
+      <Box
+        display="flex"
+        pos="fixed"
+        alignItems="center"
+        height={appBarHeight}
+        backgroundColor={colorMode === "dark" ? colors.blue : colors.white}
+        width="100%"
+        left="0"
+        right="0"
+        top="0"
+      >
+        <Container maxW={maxWidth}>
+          <Flex justify="space-between" alignItems="center">
+            <Box>
+              <Link as={RouterLink} to="/">
+                <Heading as="h1" size="md">
+                  Where in the world?
+                </Heading>
+              </Link>
+            </Box>
+            <Spacer />
+            <Box>
+              <ColorModeToggler />
+            </Box>
+          </Flex>
+        </Container>
+      </Box>
+      <Spacer height={appBarHeight} />
+    </>
+  );
+};
